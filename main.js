12527a0dca340b49f8ef555e03a13fc746d40186
Liliane F das Candeias <cigarrettes_17@yahoo.com.br>
2026-02-12 22:51:45 -0300

    Finalizacao dos dados do usuario

--- main.js
+++ main.js
@@ -2,15 +2,41 @@ document.addEventListener('DOMContentLoaded', ()=> {
     const titulo = document.querySelector('.titulo');
     const subtitulo = document.querySelector('.subtitulo');
     const bio = document.querySelector('#bio');
+    const imagem = document.querySelector('.imagem');
+    // a partir daqui
+    const blog = document.querySelector('.blog');
+    const seguidores = document.querySelector('.seguidores');
+    const seguindo = document.querySelector('.seguindo');
+    const local = document.querySelector('.local');
+    const publicRepos = document.querySelector('.publicRepos');
+    const criadoEm = document.querySelector('.criadoEm');
+    // Até aqui
+
 
     const fechData = async () =>{
         const responseUser = await fetch('https://api.github.com/users/Liicherry');
-        const dataUser = responseUser.json();
-       
-        const fectRepos = await fetch('https://api.github.com/users/Liicherry/repos');
-        const dataRepos = await fectRepos.json();
-        console.log(dataRepos);
+        const dataUser = await responseUser.json();
+
+        imagem.src = dataUser.avatar_url;
+        imagem.alt= `Imagem de ${dataUser.login}`;
+        titulo.innerHTML = dataUser.name;
+        subtitulo.innerHTML = dataUser.login;
+        bio.innerHTML = dataUser.bio;
+        if(dataUser.blog !== "") blog.innerHTML = `Blog: ${dataUser.blog}`;
+        seguidores.innerHTML = `Seguidores ${dataUser.followers}`
+        seguindo.innerHTML = `Seguindo: ${dataUser.following}`
+        if(dataUser.location !== null) local.innerHTML = `Cidade: ${dataUser.location}`
+        publicRepos.innerHTML = `Repositorios publicos ${dataUser.public_repos}`
+        criadoEm.innerHTML = `Criado em: ${new Date(dataUser.created_at).toLocaleDateString('pt-BR', {day:'2-digit', 
+            month:'long',
+            year:'numeric'})}`
+            
+            //Dados dos repositorios
+
+        const fetchRepos = await fetch ('https://api.github.com/users/Liicherry/repos');
+        const dataRepos = await fetchRepos.json();
+
     }
 
-    fechData();
+    fechData()
 })
